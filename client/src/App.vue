e958e04d7310e433bc87a45add8dd585b01b1a8b
Tamara Bain <tamara@tamaras-mbp.lan>
2020-04-23 15:19:45 +0100

    css added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,59 @@
 <template>
   <div id="app">
     <h1>Hotel Bookings!</h1>
+    <guest-form/>
+    <guest-list :guests="guests"/>
   </div>
 </template>
 
 <script>
+import {eventBus} from './main.js'
 import GuestForm from './components/GuestForm.vue';
-import GuestList from './components/GuestList.vue';
+import GuestList from './components/GuestsList.vue';
+import GuestsService from './services/GuestsService.js';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      guests: []
+    };
+  },
   components: {
     'guest-form': GuestForm,
     'guest-list': GuestList
+  },
+  mounted(){
+    GuestsService.getGuests()
+    .then(guests => this.guests = guests);
+
+    eventBus.$on('guest-added', (guest) => {
+      this.guests.push(guest)
+    })
+
+    eventBus.$on('guest-deleted', (id) => {
+      const index = this.guests.findIndex(guest => guest._id === id)
+      this.guests.splice(index, 1)
+    })
+
+    eventBus.$on('check-in-guest', (id) => {
+      const index = this.guests.findIndex(guest => guest._id === id)
+      this.guests[index].checkIn = true;
+    })
+
+    eventBus.$on('uncheck-in-guest', (id) => {
+      const index = this.guests.findIndex(guest => guest._id === id)
+      this.guests[index].checkIn = false;
+    })
   }
 }
 </script>
 
 <style>
+h1 {
+  font-size: 4em;
+  color: darkorange;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +61,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+    background-color: lightyellow;
 }
 </style>
